09e953ebb81e864b0ddd798f7591f20a5808d34b
Raul Reyes <raulreyes@users.noreply.github.com>
2021-07-19 10:47:23 +1000

    Display contact avatar, name, email, and remove button NOTE: AVATAR NOT WORKING

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -5,8 +5,18 @@ class ListContacts extends Component {
         return (
             <ol className='contact-list' >
                 {this.props.contacts.map((contact) => (
-                    <li key={contact.id}>
-                        {contact.name}
+                    <li key={contact.id} className='contact-list-item'>
+                        <div className='' style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }}
+                        ></div>
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>
+                        </div>
+                        <button className='contact-remove'>
+                            Remove
+                        </button>                   
                     </li>
                 ))}
             </ol>
